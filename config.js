149f2beb3f807740f0050639e3534b6fec6d5865
Mikael Karon <mikael@karon.se>
2015-03-09 19:03:30 +0800

    JSDoc changes.

--- config.js
+++ config.js
@@ -1,16 +1,66 @@
-define({
-  "handlers": "handlers",
-  "emitter": "emitter",
-  "type": "type",
-  "callback": "callback",
-  "data": "data",
-  "scope": "scope",
-  "executor": "executor",
-  "head": "head",
-  "tail": "tail",
-  "next": "next",
-  "count": "count",
-  "limit": "limit",
-  "on": "on",
-  "off": "off"
+define(function () {
+  /**
+   * @class mu.emitter.config
+   * @enum {String}
+   */
+  var CONFIG = {
+    /**
+     * Property name for `handlers`
+     */
+    "handlers": "handlers",
+    /**
+     * Property name for `emitter`
+     */
+    "emitter": "emitter",
+    /**
+     * Property name for `type`
+     */
+    "type": "type",
+    /**
+     * Property name for `callback`
+     */
+    "callback": "callback",
+    /**
+     * Property name for `data`
+     */
+    "data": "data",
+    /**
+     * Property name for `scope`
+     */
+    "scope": "scope",
+    /**
+     * Property name for `executor`
+     */
+    "executor": "executor",
+    /**
+     * Property name for `head`
+     */
+    "head": "head",
+    /**
+     * Property name for `tail`
+     */
+    "tail": "tail",
+    /**
+     * Property name for `next`
+     */
+    "next": "next",
+    /**
+     * Property name for `count`
+     */
+    "count": "count",
+    /**
+     * Property name for `limit`
+     */
+    "limit": "limit",
+    /**
+     * Property name for `on`
+     */
+    "on": "on",
+    /**
+     * Property name for `off`
+     */
+    "off": "off"
+  };
+
+  return CONFIG;
 });
